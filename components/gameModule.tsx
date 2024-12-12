394fce576520eb29c510243c92df5aa721458060
therealgibby <raulgarcia987@gmail.com>
2024-12-12 01:45:48 -0600

    change metadata; remove useEffect dependencies in gameModule

--- components/gameModule.tsx
+++ components/gameModule.tsx
@@ -139,7 +139,7 @@ export default function GameModule({ accessToken, playlistLink }: Props) {
 			// @ts-ignore
 			window.player.disconnect();
 		};
-	}, [accessToken, playlistURI]);
+	}, []);
 
 	return (
 		<GameView
